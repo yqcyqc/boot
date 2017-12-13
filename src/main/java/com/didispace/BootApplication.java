5ad42433a156064ec4d4187f2a4c4f749ee43a1d
yqc <anliangttt@163.com>
2017-12-13 10:09:24 +0800

    Initial commit

---
+++ src/main/java/com/didispace/BootApplication.java
@@ -0,0 +1,12 @@
+package com.didispace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+}
