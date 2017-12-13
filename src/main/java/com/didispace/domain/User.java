e54418f3de23296122225967e01f290646f1301c
yqc <anliangttt@163.com>
2017-12-13 11:01:44 +0800

    Spring Boot构建RESTful API与单元测试UserController

---
+++ src/main/java/com/didispace/domain/User.java
@@ -0,0 +1,34 @@
+package com.didispace.domain;
+
+/**
+ * Created by zhudonghong123 on 2017/12/12.
+ */
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
