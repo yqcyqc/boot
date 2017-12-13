1580b24433ee23804e4e1b9eb0f3eb07a72aefde
yqc <anliangttt@163.com>
2017-12-13 10:47:19 +0800

    Thymeleaf模板引擎

--- src/main/java/com/didispace/web/HelloController.java
+++ src/main/java/com/didispace/web/HelloController.java
@@ -1,17 +1,25 @@
 package com.didispace.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by zhudonghong123 on 2017/12/13.
  */
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+    @RequestMapping("/index")
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";
     }
 
 }
